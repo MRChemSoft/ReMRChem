0ac2288b442f6dc7d5d80e528d0294115cf17a29
Luca Frediani <luca.frediani@gmail.com>
2022-07-01 13:08:50 +0200

    Cleanup and bugfixes

--- orbital4c/NuclearPotential.py
+++ orbital4c/NuclearPotential.py
@@ -1,16 +1,23 @@
 import numpy as np
 import copy as cp
+from scipy.special import erf
 
 def CoulombPotential(position, center, charge):
-    distance = np.linalg.norm(positon - center)
+    d2 = ((position[0] - center[0])**2 +
+          (position[1] - center[1])**2 +
+          (position[2] - center[2])**2)
+    distance = np.sqrt(d2)
     potential = charge / distance
 
 def SmoothingHFYGB(Z, prec):
     factor = 0.00435 * prec / Z**5
     return factor**(1./3.)
     
-def CoulombHFYGB(position, center, charge, precison):
-    distance = np.linalg.norm(positon - center)
+def CoulombHFYGB(position, center, charge, precision):
+    d2 = ((position[0] - center[0])**2 +
+          (position[1] - center[1])**2 +
+          (position[2] - center[2])**2)
+    distance = np.sqrt(d2)
     factor = SmoothingHFYGB(charge, precision)
     value = uHFYGB(distance / factor)
     return charge * value / factor
