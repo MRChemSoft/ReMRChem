de0557ac5a046411ad338c7c2d232f368697eb49
Christian Tantardini <85489709+Christian48596@users.noreply.github.com>
2022-10-31 12:29:47 +0100

    Gaunt Direct and Exchange vectors  DONE

--- orbital4c/operators.py
+++ orbital4c/operators.py
@@ -244,27 +244,12 @@ class GauntExchangeOperator():
         
         
 
-    def __call__(self, label):
-        if label == 'spinorb1':
-            
-            cPsi_alpha  = self.cPsi[0].overlap_density(self.alpha[0], self.prec)
-            for i in range(1, len(self.cPsi)):
-                cPsi_alpha +=  self.cPsi[i].overlap_density(self.alpha[0], self.prec)
-            cPsi_alpha = cPsi_alpha
+    def __call__(self, alpha):      
+        cPsi_alpha  = self.cPsi[0].overlap_density(alpha[0], self.prec)
+        for i in range(1, len(self.cPsi)):
+            cPsi_alpha +=  self.cPsi[i].overlap_density(alpha[0], self.prec)
+        cPsi_alpha = cPsi_alpha
 
-
-        elif label == 'spinorb2':
-
-            cPsi_alpha  = self.cPsi[0].overlap_density(self.alpha[1], self.prec)
-            for i in range(1, len(self.cPsi)):
-                cPsi_alpha +=  self.cPsi[i].overlap_density(self.alpha[1], self.prec)
-            cPsi_alpha = cPsi_alpha
-
-
-        else:
-
-
-            'Invalid component'
 
         self.GK = cf.complex_fcn()
         GK_Re = self.poisson(cPsi_alpha.real) * (2.0 * np.pi)
@@ -275,13 +260,7 @@ class GauntExchangeOperator():
         GK_Im.crop(self.prec)
                         
 
-
-        if label == 'spinorb1':
-            self.potential = orb.apply_complex_potential(1.0, self.GK, self.alpha[0], self.prec)
-    
-        elif label == 'spinorb2':
-            self.potential = orb.apply_complex_potential(1.0, self.GK, self.alpha[1], self.prec)
-
+        self.potential = orb.apply_complex_potential(1.0, self.GK, self.alpha[0], self.prec)
         return self.potential
 
 
@@ -327,6 +306,7 @@ class FockMatrix2():
         energy_21, imag_21 = self.Psi[1].dot(add_psi_1)
         energy_22, imag_22 = self.Psi[1].dot(add_psi_2)
 
+        #############################################################
 
         E_H11,  imag_H1 = self.Psi[0].dot(self.J(self.Psi[0]))
         E_H12,  imag_H1 = self.Psi[0].dot(self.J(self.Psi[1]))
@@ -339,17 +319,15 @@ class FockMatrix2():
         E_xc21, imag_xc21 = self.Psi[1].dot(self.K(self.Psi[0]))
         E_xc22, imag_xc22 = self.Psi[1].dot(self.K(self.Psi[1]))
 
+        #############################################################################
 
         GJ0_1 = orb.apply_complex_potential(1.0, self.GJ0, self.alpha0[0], self.prec) 
         GJ1_1 = orb.apply_complex_potential(1.0, self.GJ1, self.alpha1[0], self.prec) 
         GJ2_1 = orb.apply_complex_potential(1.0, self.GJ2, self.alpha2[0], self.prec)
 
-
         GJ0_2 = orb.apply_complex_potential(1.0, self.GJ0, self.alpha0[1], self.prec)
         GJ1_2 = orb.apply_complex_potential(1.0, self.GJ1, self.alpha1[1], self.prec)
         GJ2_2 = orb.apply_complex_potential(1.0, self.GJ2, self.alpha2[1], self.prec)
-
-
 
         E_GH0_11,  imag_GH0_11 = self.Psi[0].dot(GJ0_1)
         E_GH1_11,  imag_GH1_11 = self.Psi[0].dot(GJ1_1)
@@ -375,34 +353,43 @@ class FockMatrix2():
         
         E_GH22 = E_GH0_22 + E_GH1_22 + E_GH2_22
 
+        #############################################################################
 
-        ###############################################
+        GK0_1 = orb.apply_complex_potential(1.0, self.GK0, self.alpha0[0], self.prec)
+        GK1_1 = orb.apply_complex_potential(1.0, self.GK1, self.alpha1[0], self.prec)
+        GK2_1 = orb.apply_complex_potential(1.0, self.GK2, self.alpha2[0], self.prec)
 
 
-        E_GK0_11,  imag_GK0_11 = self.Psi[0].dot(GJ10)
-        E_GK1_11,  imag_GK1_11 = self.Psi[0].dot(GJ11)
-        E_GK2_11,  imag_GK2_11 = self.Psi[0].dot(GJ12)
+        GK0_2 = orb.apply_complex_potential(1.0, self.GK0, self.alpha0[1], self.prec)
+        GK1_2 = orb.apply_complex_potential(1.0, self.GK1, self.alpha1[1], self.prec)
+        GK2_2 = orb.apply_complex_potential(1.0, self.GK2, self.alpha2[1], self.prec)
+
+
+        E_GK0_11,  imag_GK0_11 = self.Psi[0].dot(GK0_1)
+        E_GK1_11,  imag_GK1_11 = self.Psi[0].dot(GK1_1)
+        E_GK2_11,  imag_GK2_11 = self.Psi[0].dot(GK2_1)
 
         E_GK11 = E_GK0_11 + E_GK1_11 + E_GK2_11
         
-        E_GK0_12,  imag_GK0_12 = self.Psi[0].dot(GJ20)
-        E_GK1_12,  imag_GK1_12 = self.Psi[0].dot(GJ21)
-        E_GK2_12,  imag_GK2_12 = self.Psi[0].dot(GJ22)
+        E_GK0_12,  imag_GK0_12 = self.Psi[0].dot(GK0_2)
+        E_GK1_12,  imag_GK1_12 = self.Psi[0].dot(GK1_2)
+        E_GK2_12,  imag_GK2_12 = self.Psi[0].dot(GK2_2)
 
         E_GK12 = E_GK0_12 + E_GK1_12 + E_GK2_12
 
-        E_GK0_21,  imag_GK0_21 = self.Psi[1].dot(GJ10)
-        E_GK1_21,  imag_GK1_21 = self.Psi[1].dot(GJ11)
-        E_GK2_21,  imag_GK2_21 = self.Psi[1].dot(GJ12)
+        E_GK0_21,  imag_GK0_21 = self.Psi[1].dot(GK0_1)
+        E_GK1_21,  imag_GK1_21 = self.Psi[1].dot(GK1_1)
+        E_GK2_21,  imag_GK2_21 = self.Psi[1].dot(GK2_1)
 
         E_GK21 = E_GK0_21 + E_GK1_21 + E_GK2_21
 
-        E_GK0_22,  imag_GK0_22 = self.Psi[1].dot(GJ20)
-        E_GK1_22,  imag_GK1_22 = self.Psi[1].dot(GJ21)
-        E_GK2_22,  imag_GK2_22 = self.Psi[1].dot(GJ22)
+        E_GK0_22,  imag_GK0_22 = self.Psi[1].dot(GK0_2)
+        E_GK1_22,  imag_GK1_22 = self.Psi[1].dot(GK1_2)
+        E_GK2_22,  imag_GK2_22 = self.Psi[1].dot(GK2_2)
 
         E_GK22 = E_GK0_22 + E_GK1_22 + E_GK2_22
-     
+
+        #############################################################################     
 
         self.energy11 = energy_11 + E_H11 - E_xc11 - E_GH11 + E_GK11 
         self.energy12 = energy_12 + E_H12 - E_xc12 - E_GH12 + E_GK12
