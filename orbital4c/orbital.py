fixing the derivative

orbital4c/orbital.py
@@ -5,7 +5,6 @@ from scipy.special import gamma
 from orbital4c import complex_fcn as cf
 
 class orbital4c:
-    """Four components orbital."""
     mra = None
     light_speed = -1.0
     comp_dict = {'La': 0, 'Lb': 1, 'Sa': 2, 'Sb': 3}
@@ -166,25 +165,25 @@ class orbital4c:
         self['Sa'] = sigma_p_L[0]
         self['Sb'] = sigma_p_L[1]
         
-    def derivative(self, dir = 0, der = 'ABGV'):
-        orb_der = orbital4c()
-        for comp,func in self.comp_array.items():
-            orb_der[comp] = func.derivative(dir, der) 
-        return orb_der
-    
-    def gradient(self, der = 'ABGV'):
-        orb_grad = {}
-        for key in self.comp_dict.keys():
-            orb_grad[key] = self[key].gradient(der)
-        grad = []
-        for i in range(3):
-            comp = orbital4c()
-            comp.copy_components(La = orb_grad['La'][i], 
-                          Lb = orb_grad['Lb'][i], 
-                          Sa = orb_grad['Sa'][i], 
-                          Sb = orb_grad['Sb'][i])
-            grad.append(comp)
-        return grad
+#    def derivative(self, dir = 0, der):
+#        orb_der = orbital4c()
+#        for comp,func in self.comp_array.items():
+#            orb_der[comp] = func.derivative(dir, der) 
+#        return orb_der
+#    
+#    def gradient(self, der):
+#        orb_grad = {}
+#        for key in self.comp_dict.keys():
+#            orb_grad[key] = self[key].gradient(der)
+#        grad = []
+#        for i in range(3):
+#            comp = orbital4c()
+#            comp.copy_components(La = orb_grad['La'][i], 
+#                          Lb = orb_grad['Lb'][i], 
+#                          Sa = orb_grad['Sa'][i], 
+#                          Sb = orb_grad['Sb'][i])
+#            grad.append(comp)
+#        return grad
     
     def complex_conj(self):
         orb_out = orbital4c()
@@ -192,39 +191,39 @@ class orbital4c:
             orb_out[key] = self[key].complex_conj() 
         return orb_out
 
-    def density(self, prec):
-        density = vp.FunctionTree(self.mra)
-        add_vector = []
-        for comp in self.comp_array:
-            temp = comp.density(prec).crop(prec)
-            if(temp.squaredNorm() > 0):
-                add_vector.append((1.0,temp))
-        vp.advanced.add(prec, density, add_vector)
-        return density    
+#    def density(self, prec):
+#        density = vp.FunctionTree(self.mra)
+#        add_vector = []
+#        for comp in self.comp_array:
+#            temp = comp.density(prec).crop(prec)
+#            if(temp.squaredNorm() > 0):
+#                add_vector.append((1.0,temp))
+#        vp.advanced.add(prec, density, add_vector)
+#        return density    
+#
+#    def exchange(self, other, prec):
+#        exchange = vp.FunctionTree(self.mra)
+#        add_vector = []
+#        for comp in self.comp_dict.keys():
+#            func_i = self[comp]
+#            func_j = other[comp]
+#            temp = func_i.exchange(func_j, prec)
+#            if(temp.squaredNorm() > 0):
+#                add_vector.append((1.0,temp))    
+#        vp.advanced.add(prec, exchange, add_vector)
+#        return exchange
 
-    def exchange(self, other, prec):
-        exchange = vp.FunctionTree(self.mra)
-        add_vector = []
-        for comp in self.comp_dict.keys():
-            func_i = self[comp]
-            func_j = other[comp]
-            temp = func_i.exchange(func_j, prec)
-            if(temp.squaredNorm() > 0):
-                add_vector.append((1.0,temp))    
-        vp.advanced.add(prec, exchange, add_vector)
-        return exchange
-
-    def alpha_exchange(self, other, prec):
-        alpha_exchange = vp.FunctionTree(self.mra)
-        add_vector = []
-        for comp in self.comp_dict.keys():
-            func_i = self[comp]
-            func_j = other[comp]
-            temp = func_i.alpha_exchange(func_j, prec)
-            if(temp.squaredNorm() > 0):
-                add_vector.append((1.0,temp))    
-        vp.advanced.add(prec, alpha_exchange, add_vector)
-        return alpha_exchange    
+#    def alpha_exchange(self, other, prec):
+#        alpha_exchange = vp.FunctionTree(self.mra)
+#        add_vector = []
+#        for comp in self.comp_dict.keys():
+#            func_i = self[comp]
+#            func_j = other[comp]
+#            temp = func_i.alpha_exchange(func_j, prec)
+#            if(temp.squaredNorm() > 0):
+#                add_vector.append((1.0,temp))    
+#        vp.advanced.add(prec, alpha_exchange, add_vector)
+#        return alpha_exchange    
 
     def overlap_density(self, other, prec):
         density = cf.complex_fcn()
@@ -276,11 +275,6 @@ class orbital4c:
 #Beta c**2
     def beta(self, shift = 0):
         out_orb = orbital4c()
-#        beta = np.array([[orbital4c.light_speed**2 + shift, 0, 0, 0  ],
-#                         [0, orbital4c.light_speed**2 + shift, 0, 0  ],
-#                         [0, 0, -orbital4c.light_speed**2 + shift, 0 ],
-#                         [0, 0,  0, -orbital4c.light_speed**2 + shift]])
-#        out_orb.comp_array = beta@self.comp_array
         beta = np.array([orbital4c.light_speed**2 + shift,
                          orbital4c.light_speed**2 + shift,
                         -orbital4c.light_speed**2 + shift,
@@ -301,11 +295,11 @@ class orbital4c:
         return out_real, out_imag
 
 #    
-# here we should consider emulating the behavior of MRChem operators
-#
-def matrix_element(bra, operator, ket):
-    Opsi = operator(ket)
-    return bra.dot(Opsi)
+## here we should consider emulating the behavior of MRChem operators
+##
+#def matrix_element(bra, operator, ket):
+#    Opsi = operator(ket)
+#    return bra.dot(Opsi)
                    
 def apply_dirac_hamiltonian(orbital, prec, shift = 0.0, der = 'ABGV'):
     beta_phi = orbital.beta(shift)
@@ -401,7 +395,7 @@ def one_s_alpha_comp(x,Z,alpha,gamma_factor,norm_const,comp):
 
 def alpha_gradient(orbital, prec):
     out = orbital4c()
-    grad_vec = orbital.gradient(der = "BS")
+    grad_vec = orbital.gradient(der)
     alpha_vec = {}
     for i in range(3):
         alpha_vec[i] = grad_vec[i].alpha(i, prec)

test.py
@@ -8,7 +8,6 @@ from scipy.linalg import eig, inv
 from scipy.special import legendre, laguerre, erf, gamma
 from scipy.special import gamma
 from vampyr import vampyr3d as vp
-from vampyr import vampyr1d as vp1
 
 import argparse
 import numpy as np
@@ -46,6 +45,10 @@ if __name__ == '__main__':
                         help='put the coulomb or gaunt')
     parser.add_argument('-v', '--potential', dest='potential', type=str, default='coulomb_HFYGB',
                         help='tell me wich model for V you want to use point_charge, coulomb_HFYGB, homogeneus_charge_sphere, gaussian')
+    parser.add_argument('-s', '--save', dest='spinorb1', type=str, default='N',
+                        help='Save the FunctionTree for the alpha spinorbital called as Z')
+    parser.add_argument('-r', '--read', dest='spinorb1', type=str, default='N',
+                        help='Read the FunctionTree for the aplha spinorbital called as Z')
     args = parser.parse_args()
 
     assert args.atype != 'H', 'Please consider only atoms with more than one electron'
@@ -57,6 +60,10 @@ if __name__ == '__main__':
     assert args.potential in ['point_charge', 'smoothing_HFYGB', 'coulomb_HFYGB', 'homogeneus_charge_sphere', 'gaussian'], 'Please, specify V'
 
     assert args.deriv in ['PH', 'BS', 'ABGV'], 'Please, specify the type of derivative'
+
+        assert args.readOrbitals in ['Y', 'N'], 'Please, specify if you want (Y) or not (N) to read the FunctionTree of spinorbital.
+    
+    assert args.saveOrbitals in ['Y', 'N'], 'Please, specify if you want (Y) or not (N) to save the FunctionTree of spinorbital.
     
 
     ################# Define Paramters ###########################
@@ -79,8 +86,6 @@ if __name__ == '__main__':
     print('call MRA DONE')
     
     computeNuclearPotential = False
-    readOrbitals = True
-    runCoulomb = False
     saveOrbitals = False
     runGaunt = True
     runGaugeA = True
@@ -110,14 +115,12 @@ if __name__ == '__main__':
             V_tree = Peps(f)
         print('Define V Potential', args.potential, 'DONE')
     
-    #############################START WITH CALCULATION###################################
+    ############################# Define Spinorbitals ###################################
     
-    spinorb1 = orb.orbital4c()
-    spinorb2 = orb.orbital4c()
-    if readOrbitals:
-        spinorb1.read("spinorb1")
-        spinorb2.read("spinorb2")
-    else:
+    if args.readOrbitals == 'Y':
+        spinorb1 = orb.orbital4c()
+        spinorb1.read('args.atype')
+    elif args.readOrbitals == 'N':
         a_coeff = 3.0
         b_coeff = np.sqrt(a_coeff/np.pi)**3
         gauss = vp.GaussFunc(b_coeff, a_coeff, origin)
@@ -129,33 +132,50 @@ if __name__ == '__main__':
         complexfc.copy_fcns(real=gauss_tree)
         spinorb1.copy_components(La=complexfc)
         spinorb1.init_small_components(prec/10)
+        spinorb1.cropLargeSmall(prec)
         spinorb1.normalize()
-        spinorb2 = spinorb1.ktrs() #does this go out of scope?
+    
+    spinorb2 = spinorb1.ktrs()
+    spinorb2.cropLargeSmall(prec)
+    spinorb2.normalize()
 
-    length = 2 * args.box
+    print('Define alpha and beta spinorbitals DONE')
 
+    #############################START WITH CALCULATION###################################
     if runCoulomb:
-        spinorb1, spinorb2 = two_electron.coulomb_gs_2e(spinorb, V_tree, mra, prec)
+        two_electron.coulomb_gs_2e(spinorb1, spinorb2, V_tree, mra, prec, der, eps, E_tot_JK)
+        print('Spinor Energy =', eps - light_speed**2)
+        print('E_total(Dirac-Coulomb) =', E_tot_JK - (2.0 *light_speed**2))
     
     if runGaunt:
-        two_electron.calcGauntPert(spinorb1, spinorb2, mra, prec)
+        two_electron.calcGauntPert(spinorb1, spinorb2, mra, prec, der, gaunt)
+        print('Gaunt term =', gaunt)
     
     if runGaugeA:
-        two_electron.calcGaugePertA(spinorb1, spinorb2, mra, prec)
+        two_electron.calcGaugePertA(spinorb1, spinorb2, mra, prec, gauge2)
+        print('GaugePertA term =', gauge2)
 
     if runGaugeB:
-        two_electron.calcGaugePertB(spinorb1, spinorb2, mra, prec)
+        two_electron.calcGaugePertB(spinorb1, spinorb2, mra, prec, gauge2)
+        print('GaugePertB term =', gauge2)
 
     if runGaugeC:
-        two_electron.calcGaugePertC(spinorb1, spinorb2, mra, prec)
+        two_electron.calcGaugePertC(spinorb1, spinorb2, mra, prec, gauge2)
+        print('GaugePertC term =', gauge2)
         
     if runGaugeD:
-        two_electron.calcGaugePertD(spinorb1, spinorb2, mra, prec)
+        two_electron.calcGaugePertD(spinorb1, spinorb2, mra, prec, gauge2)
+        print('GaugePertD term =', gauge2)
         
     if runGaugeDelta:
-        two_electron.calcGaugeDelta(spinorb1, spinorb2, mra, prec)
+        two_electron.calcGaugeDelta(spinorb1, spinorb2, mra, prec, gauge2)
+        print('GaugeDelta term =', gauge2)
         
-    if saveOrbitals:
-        spinorb1.save("spinorb1")
-        spinorb2.save("spinorb2")
+    ############################# Save Spinorbitals ###################################
+        
+    if args.saveOrbitals == 'Y':
+        spinorb1.save('args.atype')
+        print('Calculation is finished saving the FunctionTree of spinorbital alpha', args.atype)
+    elif args.readOrbitals == 'N':
+        print('Calculation is finished')
     

two_electron.py
@@ -100,11 +100,6 @@ def calcGauntPert(spinorb1, spinorb2, mra, prec, gaunt):
               np.sqrt(n12[1].squaredNorm()),
               np.sqrt(n12[2].squaredNorm())
             ]
-
-    print(norm11)
-    print(norm12)
-    print(norm21)
-    print(norm22)
     
     val22 = 0
     val21 = 0
@@ -155,13 +150,7 @@ def calcPerturbationValues(contributions, P, prec, testNorm):
         val += spr + 1j * spi
     return val
     
-#
-# currently without the -1/2 factor
-# correct gauge term: multiply by -1/2
-# no delta terms from this expression
-#
-def calcGaugePertA(spinorb1, spinorb2, mra, prec):
-    print("Gauge Perturbation Version A")
+def calcGaugePertA(spinorb1, spinorb2, mra, prec, der gauge2):
     projection_operator = vp.ScalingProjector(mra, prec)
     P = vp.PoissonOperator(mra, prec)
     n11 = calcAlphaDensityVector(spinorb1, spinorb1, prec)
@@ -169,16 +158,16 @@ def calcGaugePertA(spinorb1, spinorb2, mra, prec):
     n21 = calcAlphaDensityVector(spinorb2, spinorb1, prec)
     n22 = calcAlphaDensityVector(spinorb2, spinorb2, prec)
 
-    div_n22 = cf.divergence(n22, prec)
-    div_n21 = cf.divergence(n21, prec)
+    div_n22 = cf.divergence(n22, prec, der)
+    div_n21 = cf.divergence(n21, prec, der)
     n11_dot_r = cf.vector_dot_r(n11, prec)
     n12_dot_r = cf.vector_dot_r(n12, prec)
 
     n22_r_mat = cf.vector_tensor_r(n22, prec)
     n21_r_mat = cf.vector_tensor_r(n21, prec)
 
-    grad_n11 = cf.vector_gradient(n11)
-    grad_n12 = cf.vector_gradient(n12)
+    grad_n11 = cf.vector_gradient(n11, der)
+    grad_n12 = cf.vector_gradient(n12, der)
 
     contributions = {
         "density":[n11_dot_r,
@@ -202,17 +191,10 @@ def calcGaugePertA(spinorb1, spinorb2, mra, prec):
     } 
 
     testNorm = computeTestNorm(contributions)
-    print("Test Norm A", testNorm)
-    result = calcPerturbationValues(contributions, P, prec, testNorm)
-    print("final Gauge A", result)
-    return result
+    gauge2 = -0.5 * calcPerturbationValues(contributions, P, prec, testNorm)
+    return gauge2
 
-#
-# one delta term excluded
-# most efficient method
-#
-def calcGaugePertB(spinorb1, spinorb2, mra, prec, gauge2):
-    print("Gauge Perturbation Version B")
+def calcGaugePertB(spinorb1, spinorb2, mra, prec, der, gauge2):
     projection_operator = vp.ScalingProjector(mra, prec)
     P = vp.PoissonOperator(mra, prec)
     n11 = calcAlphaDensityVector(spinorb1, spinorb1, prec)
@@ -220,8 +202,8 @@ def calcGaugePertB(spinorb1, spinorb2, mra, prec, gauge2):
     n21 = calcAlphaDensityVector(spinorb2, spinorb1, prec)
     n22 = calcAlphaDensityVector(spinorb2, spinorb2, prec)
 
-    div_n22 = cf.divergence(n22, prec)
-    div_n21 = cf.divergence(n21, prec)
+    div_n22 = cf.divergence(n22, prec, der)
+    div_n21 = cf.divergence(n21, prec, der)
     div_n22_r = cf.scalar_times_r(div_n22, prec)
     div_n21_r = cf.scalar_times_r(div_n21, prec)
     n11_dot_r = cf.vector_dot_r(n11, prec)
@@ -241,18 +223,11 @@ def calcGaugePertB(spinorb1, spinorb2, mra, prec, gauge2):
     } 
 
     testNorm = computeTestNorm(contributions)
-    gauge2 = calcPerturbationValues(contributions, P, prec, testNorm)
-    return -0.5 gauge2 
+    gauge2 = -0.5 * calcPerturbationValues(contributions, P, prec, testNorm)
+    return gauge2 
 
-#
-# currently without the -1/2 factor
-# correct gauge term: multiply by -1/2
-# one delta term excluded
-# same structure as Sun 2022
-# least efficeint method
-#
-def calcGaugePertC(spinorb1, spinorb2, mra, prec):
-    print("Gauge Perturbation Version C")
+def calcGaugePertC(spinorb1, spinorb2, mra, prec, der, gauge2):
+    print("Gauge Perturbation Version Sun 2022")
     projection_operator = vp.ScalingProjector(mra, prec)
     P = vp.PoissonOperator(mra, prec)
     n11 = calcAlphaDensityVector(spinorb1, spinorb1, prec)
@@ -260,8 +235,8 @@ def calcGaugePertC(spinorb1, spinorb2, mra, prec):
     n21 = calcAlphaDensityVector(spinorb2, spinorb1, prec)
     n22 = calcAlphaDensityVector(spinorb2, spinorb2, prec)
 
-    grad_n11 = cf.vector_gradient(n11)
-    grad_n12 = cf.vector_gradient(n12)
+    grad_n11 = cf.vector_gradient(n11, der)
+    grad_n12 = cf.vector_gradient(n12, der)
 
     grad_n11_r = [cf.vector_dot_r([grad_n11[i][0] for i in range(3)], prec),
                   cf.vector_dot_r([grad_n11[i][1] for i in range(3)], prec),
@@ -275,8 +250,8 @@ def calcGaugePertC(spinorb1, spinorb2, mra, prec):
     
     
 
-    div_n22 = cf.divergence(n22, prec)
-    div_n21 = cf.divergence(n21, prec)
+    div_n22 = cf.divergence(n22, prec, der)
+    div_n21 = cf.divergence(n21, prec, der)
     div_n22_r = cf.scalar_times_r(div_n22, prec)
     div_n21_r = cf.scalar_times_r(div_n21, prec)
     n11_dot_r = cf.vector_dot_r(n11, prec)
@@ -306,18 +281,10 @@ def calcGaugePertC(spinorb1, spinorb2, mra, prec):
     } 
 
     testNorm = computeTestNorm(contributions)
-    print("Test Norm C", testNorm)
-    result = calcPerturbationValues(contributions, P, prec, testNorm)
-    print("final Gauge C", result)
-    return result
+    gauge2 = -0.5 * calcPerturbationValues(contributions, P, prec, testNorm)
+    return gauge2
 
-#
-# currently without the -1/2 factor
-# correct gauge term: multiply by -1/2
-# double delta term excluded
-#
-def calcGaugePertD(spinorb1, spinorb2, mra, prec):
-    print("Gauge Perturbation Version D")
+def calcGaugePertD(spinorb1, spinorb2, mra, prec, der, gauge2):
     projection_operator = vp.ScalingProjector(mra, prec)
     P = vp.PoissonOperator(mra, prec)
     n11 = calcAlphaDensityVector(spinorb1, spinorb1, prec)
@@ -325,8 +292,8 @@ def calcGaugePertD(spinorb1, spinorb2, mra, prec):
     n21 = calcAlphaDensityVector(spinorb2, spinorb1, prec)
     n22 = calcAlphaDensityVector(spinorb2, spinorb2, prec)
 
-    grad_n11 = cf.vector_gradient(n11)
-    grad_n12 = cf.vector_gradient(n12)
+    grad_n11 = cf.vector_gradient(n11, der)
+    grad_n12 = cf.vector_gradient(n12, der)
         
     grad_n11_r = [cf.vector_dot_r([grad_n11[i][0] for i in range(3)], prec),
                   cf.vector_dot_r([grad_n11[i][1] for i in range(3)], prec),
@@ -336,8 +303,8 @@ def calcGaugePertD(spinorb1, spinorb2, mra, prec):
                   cf.vector_dot_r([grad_n12[i][1] for i in range(3)], prec),
                   cf.vector_dot_r([grad_n12[i][2] for i in range(3)], prec)]
 
-    div_n22 = cf.divergence(n22, prec)
-    div_n21 = cf.divergence(n21, prec)
+    div_n22 = cf.divergence(n22, prec, der)
+    div_n21 = cf.divergence(n21, prec, der)
 
     div_n22_r = cf.scalar_times_r(div_n22, prec)
     div_n21_r = cf.scalar_times_r(div_n21, prec)
@@ -362,16 +329,10 @@ def calcGaugePertD(spinorb1, spinorb2, mra, prec):
     } 
 
     testNorm = computeTestNorm(contributions)
-    print("Test Norm D", testNorm)
-    result = calcPerturbationValues(contributions, P, prec, testNorm)
-    print("final Gauge D", result)
-    return result
+    gauge = -0.5 * calcPerturbationValues(contributions, P, prec, testNorm)
+    return gauge2
 
-#
-# gauge delta term
-#
 def calcGaugeDelta(spinorb1, spinorb2, mra, prec, gauge1):
-    print("Gauge Perturbation Delta")
     projection_operator = vp.ScalingProjector(mra, prec)
     P = vp.PoissonOperator(mra, prec)
     n11 = calcAlphaDensityVector(spinorb1, spinorb1, prec)
@@ -388,5 +349,5 @@ def calcGaugeDelta(spinorb1, spinorb2, mra, prec, gauge1):
     } 
 
     testNorm = computeTestNorm(contributions)
-    gauge1 = calcPerturbationValues(contributions, P, prec, testNorm)
-    return -0.5 * gauge1
+    gauge1 = -0.5 * calcPerturbationValues(contributions, P, prec, testNorm)
+    return gauge1