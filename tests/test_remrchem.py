4dc9e63dfeb72d10c3b4a41644ff69921c4ef51c
Luca Frediani <luca.frediani@gmail.com>
2023-05-24 13:43:11 +0200

    WIP

--- tests/test_remrchem.py
+++ tests/test_remrchem.py
@@ -35,25 +35,20 @@ def test_spinor():
     print(val)
     assert val == pytest.approx(0.5937902746013326)
 
-def test_read():
-    print("test_read")
-    spinorb1 = orb.orbital4c()
-    spinorb2 = orb.orbital4c()
-    spinorb1.read("trees/spinorb1")
-    spinorb2.read("trees/spinorb2")
-    val1 = spinorb1.comp_array[0].real([0.0, 0.0, 0.0])
-    val2 = spinorb2.comp_array[3].imag([0.0, 0.0, 0.0])
-    print(val1, val2)
-    assert val1 == pytest.approx(1.3767534073967547)
-    assert val2 == pytest.approx(-0.012619848367561309)
+#def test_read():
+#    print("test_read")
+#    spinorb1 = orb.orbital4c()
+#    spinorb2 = orb.orbital4c()
+#    spinorb1.read("trees/spinorb1")
+#    spinorb2.read("trees/spinorb2")
+#    val1 = spinorb1.comp_array[0].real([0.0, 0.0, 0.0])
+#    val2 = spinorb2.comp_array[3].imag([0.0, 0.0, 0.0])
+#    print(val1, val2)
+#    assert val1 == pytest.approx(1.3767534073967547)
+#    assert val2 == pytest.approx(-0.012619848367561309)
 
 #def test_mul():
 #    print("test_mul")
 #    spinorb1 = orb.orbital4c()
-    
-
-#if __name__ == '__main__':
-#    test_spinor()
-#    test_read()
 
     
