3bc4818135634611340fb5920eb7f7619b554adc
Luca Frediani <luca.frediani@gmail.com>
2023-10-27 18:16:57 +0200

    1. Some output improvements 2. Modulation of precision for the D2 method

--- one_electron.py
+++ one_electron.py
@@ -17,94 +17,119 @@ def analytic_1s(light_speed, n, k, Z):
     tmp3 = 1 + tmp2**2
     return light_speed**2 / np.sqrt(tmp3)
 
-def gs_D_1e(spinorb1, potential, mra, prec, derivative):
-    print('Hartree-Fock 1e')
+def gs_D_1e(spinorb1, potential, mra, prec, thr, derivative):
+    print('One-electron calculations')
     
     error_norm = 1
     #compute_last_energy = False
 
     light_speed = spinorb1.light_speed
-
-    while error_norm > prec:
+    old_energy = 0
+    
+    while error_norm > thr:
         hd_psi = orb.apply_dirac_hamiltonian(spinorb1, prec, der = derivative)
         v_psi = orb.apply_potential(-1.0, potential, spinorb1, prec)
         add_psi = hd_psi + v_psi
         energy = spinorb1.dot(add_psi).real
-        print('Energy',energy - light_speed**2)
         mu = orb.calc_dirac_mu(energy, light_speed)
         tmp = orb.apply_helmholtz(v_psi, mu, prec)
-        tmp.crop(prec/10)
+#        tmp = orb.apply_dirac_hamiltonian(v_psi, prec, energy, der = derivative)
+        tmp.cropLargeSmall(prec)
         new_orbital = orb.apply_dirac_hamiltonian(tmp, prec, energy, der = derivative)
-        new_orbital.crop(prec/10)
+#        new_orbital =  orb.apply_helmholtz(tmp, mu, prec)
+#        new_orbital.cropLargeSmall(prec)
+        new_orbital.cropLargeSmall(prec)
         new_orbital.normalize()
         delta_psi = new_orbital - spinorb1
         #orbital_error = delta_psi.dot(delta_psi).real
         deltasq = delta_psi.squaredNorm()
         error_norm = np.sqrt(deltasq)
         print('Error', error_norm)
+        print('Delta E', energy - old_energy)
+        print('Energy',energy - light_speed**2)
+        old_energy = energy
         spinorb1 = new_orbital
     
     hd_psi = orb.apply_dirac_hamiltonian(spinorb1, prec, der = derivative)
     v_psi = orb.apply_potential(-1.0, potential, spinorb1, prec)
     add_psi = hd_psi + v_psi
     energy = spinorb1.dot(add_psi).real
-    print('Final Energy',energy - light_speed**2)
+    energy_1s = analytic_1s(light_speed, 1, -1, 1)
+    print("Exact energy: ", energy_1s - light_speed**2)
+    print('Final Energy:',energy - light_speed**2)
+    print('Delta Energy:',energy - old_energy)
+    print('Error Energy:',energy - energy_1s)
     return spinorb1
 
 
-def gs_D2_1e(spinorb1, potential, mra, prec, derivative):
+def gs_D2_1e(spinorb1, potential, mra, prec, thr, derivative):
     print('Hartree-Fock 1e D2')
     error_norm = 1
     light_speed = spinorb1.light_speed
     c2 = light_speed * light_speed
-
-    while error_norm > prec:
+    old_energy = 0
+    while error_norm > thr:
         v_psi = orb.apply_potential(-1.0, potential, spinorb1, prec) 
-        vv_psi = orb.apply_potential(-0.5/c2, potential, v_psi, prec)
+        vv_psi = orb.apply_potential(-0.5/c2, potential, v_psi, prec*c2)
         beta_v_psi = v_psi.beta2()
         apV_psi = v_psi.alpha_p(prec, derivative)
-        ap_psi = spinorb1.alpha_p(prec, derivative)
-        Vap_psi = orb.apply_potential(-1.0, potential, ap_psi, prec)
+        ap_psi = spinorb1.alpha_p(prec*light_speed, derivative)
+        Vap_psi = orb.apply_potential(-1.0, potential, ap_psi, prec*light_speed)
         anticom = apV_psi + Vap_psi
-        anticom.cropLargeSmall(prec)
-        #beta_v_psi.cropLargeSmall(prec)
-        vv_psi.cropLargeSmall(prec)
+#        anticom.cropLargeSmall(prec)
+#        beta_v_psi.cropLargeSmall(prec)
+#        vv_psi.cropLargeSmall(prec)
         RHS = beta_v_psi + vv_psi + anticom * (0.5/light_speed)
-        #RHS.cropLargeSmall(prec)
+        RHS.cropLargeSmall(prec)
         cke = spinorb1.classicT()
         cpe = (spinorb1.dot(RHS)).real
         print("Classic-like energies:", "cke =", cke,"cpe =", cpe,"cke + cpe =", cke + cpe)
+        classic_energy = cke + cpe
+        energy = c2*(np.sqrt(1+2*classic_energy/c2)-1)
         mu = orb.calc_non_rel_mu(cke+cpe)
         print("mu =", mu)
         new_orbital = orb.apply_helmholtz(RHS, mu, prec)
+        new_orbital.cropLargeSmall(prec)
         new_orbital.normalize()
         delta_psi = new_orbital - spinorb1
         deltasq = delta_psi.squaredNorm()
         error_norm = np.sqrt(deltasq)
         print("Error =", error_norm)
+        print('Delta E', energy - old_energy)
+        print('Energy',energy, old_energy)
+        old_energy = energy
         spinorb1 = new_orbital
     
     hd_psi = orb.apply_dirac_hamiltonian(spinorb1, prec, der = derivative)
     v_psi = orb.apply_potential(-1.0, potential, spinorb1, prec)
     add_psi = hd_psi + v_psi
-    energy = spinorb1.dot(add_psi).real
+    energy_dirac = spinorb1.dot(add_psi).real
     
-    cke = spinorb1.classicT()
+#    v_psi = orb.apply_potential(-1.0, potential, spinorb1, prec) 
+    vv_psi = orb.apply_potential(-0.5/c2, potential, v_psi, prec*c2)
     beta_v_psi = v_psi.beta2()
-    beta_pot = (beta_v_psi.dot(spinorb1)).real
-    pot_sq  = (v_psi.dot(v_psi)).real
-    ap_psi = spinorb1.alpha_p(prec, derivative)
-    anticom = (ap_psi.dot(v_psi)).real
-    energy_kutzelnigg = cke + beta_pot + pot_sq/(2*c2) + anticom/light_speed
-    
-    print('Kutzelnigg =',cke, beta_pot, pot_sq/(2*c2), anticom/light_speed, energy_kutzelnigg)
-    print('Quadratic approx =',energy_kutzelnigg - energy_kutzelnigg**2/(2*c2))
-    print('Correct from Kutzelnigg =', c2*(np.sqrt(1+2*energy_kutzelnigg/c2)-1))
-    print('Final Energy =',energy - light_speed**2)
-    
+    apV_psi = v_psi.alpha_p(prec*light_speed, derivative)
+    ap_psi = spinorb1.alpha_p(prec*light_speed, derivative)
+    Vap_psi = orb.apply_potential(-1.0, potential, ap_psi, prec)
+    anticom = apV_psi + Vap_psi
+#    anticom.cropLargeSmall(prec)
+#    beta_v_psi.cropLargeSmall(prec)
+#    vv_psi.cropLargeSmall(prec)
+    RHS = beta_v_psi + vv_psi + anticom * (0.5/light_speed)
+    RHS.cropLargeSmall(prec)
+    cke = spinorb1.classicT()
+    cpe = (spinorb1.dot(RHS)).real
+    classic_energy = cke + cpe
+    energy = c2*(np.sqrt(1+2*classic_energy/c2)-1)
     energy_1s = analytic_1s(light_speed, 1, -1, 1)
     
-    print('Exact Energy =',energy_1s - light_speed**2)
-    print('Difference 1 =',energy_1s - energy)
-    print('Difference 2 =',energy_1s - energy_kutzelnigg - light_speed**2)
+#    print('Kutzelnigg =',cke, cpe, energy_kutzelnigg)
+#    print('Quadratic approx =',energy_kutzelnigg - energy_kutzelnigg**2/(2*c2))
+    print('Exact Energy = ', energy_1s - light_speed**2)
+    print('Dirac Energy = ', energy_dirac - light_speed**2)
+    print('Kutze Energy = ', energy)
+    print('Error Kutze  = ', energy - energy_1s + light_speed**2)
+    print('Error Dirac  = ', energy_dirac - energy_1s)
+    print('Delta Energy = ', energy - old_energy)
+    print('Dirac - Kutzelnigg = ', energy_dirac - energy - light_speed**2)   
     return spinorb1
