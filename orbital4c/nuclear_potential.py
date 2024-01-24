49ebf67359b63935055d9392eb83ece349cd7031
Luca Frediani <luca.frediani@gmail.com>
2024-01-24 16:14:20 +0100

    moved two function definitions out of a third function

--- orbital4c/nuclear_potential.py
+++ orbital4c/nuclear_potential.py
@@ -116,17 +116,19 @@ def point_charge(position, center , charge):
     distance = np.sqrt(d2)
     return charge / distance
 
+def smoothing_HFYGB(charge, prec):
+    factor = 0.00435 * prec / charge**5
+    return factor**(1./3.)
+
+def uHFYGB(r):
+    u = erf(r)/r + (1/(3*np.sqrt(np.pi)))*(np.exp(-(r**2)) + 16*np.exp(-4*r**2))
+    return u
+
 def coulomb_HFYGB(position, center, charge, precision):
     d2 = ((position[0] - center[0])**2 +
           (position[1] - center[1])**2 +
           (position[2] - center[2])**2)
     distance = np.sqrt(d2)
-    def smoothing_HFYGB(charge, prec):
-        factor = 0.00435 * prec / charge**5
-        return factor**(1./3.)
-    def uHFYGB(r):
-        u = erf(r)/r + (1/(3*np.sqrt(np.pi)))*(np.exp(-(r**2)) + 16*np.exp(-4*r**2))
-        return u
     factor = smoothing_HFYGB(charge, precision)
     value = uHFYGB(distance/factor)
     return charge * value / factor
